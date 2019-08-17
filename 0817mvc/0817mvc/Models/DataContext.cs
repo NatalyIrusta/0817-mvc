b680a1b393d561e5084a7409eb29a14bdd431dcb
visual_community@outlook.com <visual_community@outlook.com>
2019-08-17 08:49:21 -0400

    con MVC Completo

---
+++ 0817mvc/0817mvc/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace _0817mvc.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<_0817mvc.Models.Student> Students { get; set; }
+    }
+}
